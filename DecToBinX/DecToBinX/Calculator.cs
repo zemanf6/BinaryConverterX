c444b35a6367f496bb86b5707428222606cd457e
zeman.fi6 <zeman.fi6@gmail.com>
2020-09-12 19:34:32 +0200

    Přidejte soubory projektu.

---
+++ DecToBinX/DecToBinX/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecToBinX
+{
+    class Calculator
+    {
+        public string Process { get; set; }
+        internal string Input(double numberInput)
+        {
+            Process = "";
+            numberInput = Math.Floor(numberInput);
+            string numberOutput = "";
+            string modulo;
+            if (numberInput != 0)
+            {
+
+            }
+            while (numberInput != 1)
+            {
+                if (numberInput % 2 == 0) //even
+                {
+                    numberOutput += "0";
+                    modulo = "0";
+                }
+
+                else //odd
+                {
+                    numberOutput += "1";
+                    modulo = "1";
+                }
+
+                Process += $"{numberInput} | {modulo} \n";
+                numberInput = Math.Floor(numberInput /= 2);
+            }
+            Process += "0 | 1";
+            numberOutput += "1";
+
+            return Reverse(numberOutput);
+        }
+
+        private string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ DecToBinX/DecToBinX/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DecToBinX
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        readonly Calculator calculator = new Calculator();
+
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            button.Clicked += Button_Clicked;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(entry.Text, out double temp))
+            {
+                if (temp < 1)
+                {
+                    result.Text = temp.ToString();
+                    process.Text = "...";
+                }
+                else
+                {
+                    result.Text = calculator.Input(temp);
+                    process.Text = calculator.Process;
+                }
+            }
+
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length > 30)
+            {
+                entry.Text = entry.Text.Remove(30);
+            }
+        }
+    }
+}
